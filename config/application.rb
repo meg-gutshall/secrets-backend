86f1301d692307cb2acaca73d769d65245e10ecc
Meg Gutshall <meghan.gutshall@gmail.com>
2021-06-28 14:17:32 -0400

    Add generators config

--- config/application.rb
+++ config/application.rb
@@ -36,5 +36,11 @@ module SecretsBackend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.generators do |g|
+      g.test_framework = nil
+      g.scss = nil
+      g.assets = nil
+      g.jbuilder = nil
+    end
   end
 end
