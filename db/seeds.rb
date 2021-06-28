3e2d9f383186d1740bfaf627e2f7e943e1912b64
Meg Gutshall <meghan.gutshall@gmail.com>
2021-06-28 14:41:20 -0400

    Create Secret seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+Secret.destroy_all
+
+Secret.create!(body: "I wear my socks 3 times before washing them.")
+Secret.create!(body: "I cheated on my wife, but it was only one time.")
+Secret.create!(body: "I never wear underware.")
+Secret.create!(body: "I love the smell of my own farts.")
+
+puts "Created #{Secret.all.count} secrets"
