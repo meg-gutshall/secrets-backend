7381bf14b576d4b859ea68f1f1bc24fcdeff29da
Meg Gutshall <meghan.gutshall@gmail.com>
2021-06-28 15:25:55 -0400

    Fix spelling error

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@ Secret.destroy_all
 
 Secret.create!(body: "I wear my socks 3 times before washing them.")
 Secret.create!(body: "I cheated on my wife, but it was only one time.")
-Secret.create!(body: "I never wear underware.")
+Secret.create!(body: "I never wear underwear.")
 Secret.create!(body: "I love the smell of my own farts.")
 
 puts "Created #{Secret.all.count} secrets"
