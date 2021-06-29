fba012b8ed5509327d0d6b4556310b2762c88f44
Meg Gutshall <meghan.gutshall@gmail.com>
2021-06-29 15:06:27 -0400

    Fix Secrets controller

--- app/controllers/api/secrets_controller.rb
+++ app/controllers/api/secrets_controller.rb
@@ -3,45 +3,45 @@ class Api::SecretsController < ApplicationController
 
   # GET /secrets
   def index
-    @secrets = Secret.all
+    secrets = Secret.all
 
-    render json: SecretSerializer.new(@secrets)
+    render json: SecretSerializer.new(secrets)
   end
 
   # GET /secrets/1
   def show
-    render json: SecretSerializer.new(@secrets)
+    render json: SecretSerializer.new(secret)
   end
 
   # POST /secrets
   def create
-    @secret = Secret.new(secret_params)
+    secret = Secret.new(secret_params)
 
-    if @secret.save
-      render json: SecretSerializer.new(@secrets), status: :created, location: @secret
+    if secret.save
+      render json: SecretSerializer.new(secret), status: :created
     else
-      render json: @secret.errors, status: :unprocessable_entity
+      render json: secret.errors, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /secrets/1
   def update
-    if @secret.update(secret_params)
-      render json: SecretSerializer.new(@secrets)
+    if secret.update(secret_params)
+      render json: SecretSerializer.new(secret)
     else
-      render json: @secret.errors, status: :unprocessable_entity
+      render json: secret.errors, status: :unprocessable_entity
     end
   end
 
   # DELETE /secrets/1
   def destroy
-    @secret.destroy
+    secret.destroy
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_secret
-      @secret = Secret.find(params[:id])
+      secret = Secret.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
